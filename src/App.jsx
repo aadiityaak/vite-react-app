31d724187ea3ff58b01ae5feb893cd7710f1611c
aadiityaak <aadiityaak@gmail.com>
2022-10-24 07:53:18 +0700

    Add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Card from './components/Card';
+import Button from './components/Button';
 
-export default function App() {
+function App() {
     return (
-        <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
-            <div className={'flex gap-x-2 place-content-center-center'}>
+        <div className={'bg-slate-900 text-slate-800  grid place-content-center min-h-screen'}>
+            <div className={'max-w-2xl flex gap-4 items-center w-full'}>
                 <Card>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis optio quasi est sed soluta commodi ex tempora, nobis omnis,
-                    voluptas obcaecati. Vel aperiam iure sapiente voluptas inventore nulla, delectus illo.
+                    <Card.Title>Halllo</Card.Title>
+                    <Card.Body>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis optio quasi est sed soluta commodi ex tempora, nobis omnis,
+                        voluptas obcaecati. Vel aperiam iure sapiente voluptas inventore nulla, delectus illo.
+                    </Card.Body>
+                    
+                    <Card.Footer><Button>Register</Button></Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Title>Halllo</Card.Title>
+                    <Card.Body>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis optio quasi est sed soluta commodi ex tempora, nobis omnis,
+                        voluptas obcaecati. Vel aperiam iure sapiente voluptas inventore nulla, delectus illo.
+                    </Card.Body>
+                    
+                    <Card.Footer><Button>Register</Button></Card.Footer>
                 </Card>
             </div>
         </div>
     );
 }
+
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,36 @@
 function Card({ children }) {
-    return <div>{children}</div>;
+    return <div className={'shadow rounded-lg bg-white overflow-hidden'}>{children}</div>;
 }
+
+function Title({children}) {
+    return (
+        <div className="p-4 border-b">
+            <h1 className="text-xl">
+                {children}
+            </h1>
+        </div>
+
+    )
+}
+
+function Body({children}){
+    return (
+        <div className={'leading-relaxed p-4'}>
+            {children}
+        </div>
+    )
+}
+
+function Footer({children}){
+    return (
+        <div className={'bg-slate-50 p-4'}>
+            {children}
+        </div>
+    )
+}
+
+Card.Title = Title;
+Card.Body = Body;
+Card.Footer = Footer;
+
 export default Card;
